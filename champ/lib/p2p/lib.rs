e9bb086260ee77ef568e00c820f6faa472997659
Malox10 <Maxi.Junges@gmx.de>
2021-11-11 17:40:17 +0100

    initial structure

--- champ/lib/p2p/lib.rs
+++ champ/lib/p2p/lib.rs
@@ -1,1 +1,26 @@
 mod connection;
+
+#[allow(dead_code)]
+struct P2pClient {
+    config: P2pConfig
+}
+
+#[allow(dead_code)]
+impl P2pClient {
+    pub fn new(&self, config: P2pConfig) -> P2pClient {
+        P2pClient { config }
+    }
+
+    pub async fn start(&mut self) {
+        //do starting stuff
+    }
+}
+
+#[allow(dead_code)]
+//holds different config values for the P2pClient
+pub struct P2pConfig {
+    max_number_connections: i32,
+    max_incoming_connections: i32,
+    listening_port: String,
+    is_delegate: bool,
+}
